da8fca9442cbd97fba6978b05d73d06e264c4c21
Tăng Công Tổng <tangcongtong271201@gmail.com>
2023-01-10 01:42:47 +0700

    link navlink

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import logo from "./logo.svg";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link, NavLink } from "react-router-dom";
 import "./App.css";
 import AlbumFeature from "./features/Album";
 import TodoFeature from "./features/Todo";
@@ -11,10 +11,27 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>Tăng Công Tổng</p>
       </header> */}
-      Homepage
+      <h1>Homepage</h1>
+      <h5>Header</h5>
+        <p>
+          <Link to="/todos">Todos</Link>
+        </p>
+        <p>
+          <Link to="/albums">Albums</Link>
+        </p>
+
+        <p>
+          <NavLink to="/todos"   className={({ isActive }) => (isActive ? 'active' : 'inactive')}>
+            Todos
+          </NavLink>
+        </p>
+        <p>
+          <NavLink to="/albums">Albums</NavLink>
+        </p>
       <Routes>
+
         <Route path="/todos" element={<TodoFeature />}></Route>
-        <Route path="/aaa" element={<AlbumFeature />}></Route>
+        <Route path="/albums" element={<AlbumFeature />}></Route>
       </Routes>
       Footer
     </div>
