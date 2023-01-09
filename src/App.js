1e2959bf9fe5b0ccdb9e7a77bd7fd242914fc157
Tăng Công Tổng <tangcongtong271201@gmail.com>
2023-01-10 01:13:20 +0700

    create routing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import logo from "./logo.svg";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import AlbumFeature from "./features/Album";
 import TodoFeature from "./features/Todo";
@@ -6,13 +7,16 @@ import TodoFeature from "./features/Todo";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Tăng Công Tổng</p>
-      </header>
-      <AlbumFeature>
-      </AlbumFeature>
-      <TodoFeature></TodoFeature>
+      </header> */}
+      Homepage
+      <Routes>
+        <Route path="/todos" element={<TodoFeature />}></Route>
+        <Route path="/aaa" element={<AlbumFeature />}></Route>
+      </Routes>
+      Footer
     </div>
   );
 }
